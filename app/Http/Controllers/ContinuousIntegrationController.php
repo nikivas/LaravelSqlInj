d1dc9e4daffc7f25cb5bf1137ab7ca6b0e30f626
Nikivas <nikivas97@mail.ru>
2019-04-11 11:08:14 +0400

    rewrite on exec

--- app/Http/Controllers/ContinuousIntegrationController.php
+++ app/Http/Controllers/ContinuousIntegrationController.php
@@ -9,8 +9,7 @@ class ContinuousIntegrationController extends Controller
  
     public function index()
     {
-        shell_exec('cd /var/www/ctf_sql_inj/ && git stash ' );
-        $resultMessage = shell_exec('bash -c "cd /var/www/ctf_sql_inj/ && git pull" ' );
-        return $resultMessage;
+        exec('bash -c "cd /var/www/ctf_sql_inj/ && git stash && git pull" ' );
+        return "done";
     }
 }
