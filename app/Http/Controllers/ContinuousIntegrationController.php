381f193a757235cead8a794b5699a8e040f4f252
Nikivas <nikivas97@mail.ru>
2019-04-11 10:43:29 +0400

    path fix

--- app/Http/Controllers/ContinuousIntegrationController.php
+++ app/Http/Controllers/ContinuousIntegrationController.php
@@ -9,9 +9,9 @@ class ContinuousIntegrationController extends Controller
  
     public function index()
     {
-        shell_exec( 'cd /var/www/asuctf/ && git stash ' );
-        $resultMessage = shell_exec( 'cd /var/www/asuctf/ && git stash ' );
-        $resultMessage .= shell_exec('cd /var/www/asuctf/ && git pull ');
+        shell_exec( 'cd /var/www/ctf_sql_inj/ && git stash ' );
+        $resultMessage = shell_exec( 'cd /var/www/ctf_sql_inj/ && git stash ' );
+        $resultMessage .= shell_exec('cd /var/www/ctf_sql_inj/ && git pull ');
         return $resultMessage;
     }
 }
