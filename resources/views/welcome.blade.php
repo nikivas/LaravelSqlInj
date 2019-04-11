2ecce76e5e808a096a4a136ded37d463d5403cc8
Nikivas <nikivas97@mail.ru>
2019-04-11 11:06:02 +0400

    welocme front

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -85,7 +85,7 @@
                 </div>
 
                 <div class="links">
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="https://github.com/nikivas">GitHub</a>
                 </div>
             </div>
         </div>
